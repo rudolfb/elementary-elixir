8a6331fc6c97bb60f3fd20921b49db52e3bf49ef
Rudolf Bargholz <rudolf@bargholz.ch>
2020-02-13 16:45:36 +0100

    Update to latest versions

--- post_install.sh
+++ post_install.sh
@@ -3,14 +3,14 @@ asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
 
 # Erlang
-asdf install erlang 22.2.1
-asdf global erlang 22.2.1
+asdf install erlang 22.2.6
+asdf global erlang 22.2.6
 
 # Elixir
-asdf install elixir 1.9.4-otp-22
-asdf global elixir 1.9.4-otp-22
+asdf install elixir 1.10.1-otp-22
+asdf global elixir 1.10.1-otp-22
 
 # Phoenix
 mix do local.hex --force , \
      local.rebar --force , \
-     archive.install hex phx_new 1.4.11
+     archive.install hex phx_new 1.4.13
